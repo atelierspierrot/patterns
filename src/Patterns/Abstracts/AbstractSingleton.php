9fb070d4d4c5bf6aa146ba2a5c772b45b95c7da1
Piero Wbmstr <me@e-piwi.fr>
2013-04-01 00:37:26 +0200

    The AbstractSingleton is reviewed
    The class is redesigned for a better management of the registry of
    instances (they are now created outside the "getInstance" method) - I
    tried to comment the whole class ... so just have a look ;)

--- src/Patterns/Abstracts/AbstractSingleton.php
+++ src/Patterns/Abstracts/AbstractSingleton.php
@@ -14,71 +14,118 @@ use \ReflectionClass, \Exception;
 /**
  * A singleton abstract class
  *
+ * No abstract method is defined but the best practice is to define an `init()` method in
+ * the child class as it will be called after instance creation. Arguments passed to the
+ * `getInstance` and `getInstanceByClassname` methods will be transmitted to the `__construct`
+ * or `init` method.
+ *
+ * The complete stack trace for an instance creation is:
+ *
+ * - if the constructor is public (*bad practice*):
+ *
+ *       $class->__construct( arguments )
+ *       $class->init()
+ *
+ * - if the constructor is not public (*good practice*):
+ *
+ *       $class = new classname
+ *       $class->init( arguments )
+ *
  * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
  */
 abstract class AbstractSingleton 
 {
 
+    /**
+     * The instances stack
+     */
     private static $_instances = array();
 
+    /**
+     * Constructor : classic object constructor
+     *
+     * The best practice is to define a `private` constructor to avoid object direct 
+     * creation ; if the constructor is public, it will receive the arguments passed
+     * to the `getInstance` method.
+     */
     private function __construct(){}
 
+    /**
+     * Initializer : this method is called after any instance creation
+     *
+     * This method will receive the arguments passed to the `getInstance` method while
+     * creating a new instance ; you can use it to define a special constructor with
+     * required arguments.
+     */
     protected function init(){}
 
+    /**
+     * Static object getter: creation of an instance of the current calling class
+     *
+     * @param any You can define as many parameters as wanted, they will be passed to
+     *            the `__construct` or `init` methods
+     * @return object This may return an instance of an object
+     */
     public static function &getInstance() 
     {
         $classname = get_called_class(); 
         if (!isset(self::$_instances[ $classname ])) {
-            $reflection_obj = new ReflectionClass($classname);
-            $callable = $reflection_obj->getMethod('__construct')->isPublic();
-            if ($callable) {
-                self::$_instances[ $classname ] = call_user_func_array(array($reflection_obj, 'newInstance'), func_get_args());
-            } else {
-                self::$_instances[ $classname ] = new $classname;
-            }
-            self::$_instances[ $classname ]->init();
+            self::createInstance($classname, func_get_args());
         }
         $instance = self::$_instances[ $classname ];
         return $instance;
     }
 
+    /**
+     * Static object getter: creation of an instance from the class name passed in first argument
+     *
+     * @param string $classname The classname to create object from
+     * @param any You can define as many parameters as wanted, they will be passed to
+     *            the `__construct` or `init` methods
+     * @return object This may return an instance of an object
+     */
+    public static function &getInstanceByClassname($classname) 
+    {
+        $arguments = func_get_args();
+        array_shift($arguments);
+        if (!isset(self::$_instances[ $classname ])) {
+            self::createInstance($classname, $arguments);
+        }
+        $instance = self::$_instances[ $classname ];
+        return $instance;
+    }
+
+    /**
+     * Real object instances creation
+     *
+     * @param string $classname The classname to create object from
+     * @param array $arguments The arguments to pass to the `__construct` or `init` method
+     * @return void
+     */
+    private static function createInstance($classname, array $arguments = array())
+    {
+        $reflection_obj = new ReflectionClass($classname);
+        if ($reflection_obj->getMethod('__construct')->isPublic()) {
+            self::$_instances[ $classname ] = call_user_func_array(array($reflection_obj, 'newInstance'), $arguments);
+            self::$_instances[ $classname ]->init();
+        } else {
+            self::$_instances[ $classname ] = new $classname;
+            call_user_func_array(array(self::$_instances[ $classname ], 'init'), $arguments);
+        }
+    }
+
+    /**
+     * Object cloning is avoid
+     *
+     * @throws Exception Trying to clone a Singleton object will throw an Excpetion
+     */
     public function __clone()
     {
         throw new Exception(
-            sprintf('Cloning of a "%s" instance is not allowed!', get_called_class())
+            sprintf('Cloning a "%s" instance is not allowed!', get_called_class())
         );
     }
 
 }
-/*
-abstract class AbstractSingleton 
-{
 
-	private static $_instance = array();
-
-	private function __construct(){}
-
-	protected function init(){}
-
-	public static function &getInstance() 
-	{
-	   $classname = get_called_class(); 
-	   if (!isset(self::$_instance[ $classname ])) 
-	   {
-		  self::$_instance[ $classname ] = new $classname();
-		  self::$_instance[ $classname ]->init();
-	   }
-	   $instance = self::$_instance[ $classname ];
-	   return $instance;
-	}
-	
-    public function __clone()
-    {
-        throw new Exception(
-            sprintf('Cloning of a "%s" instance is not allowed!', get_called_class())
-        );
-    }
-
-}
-*/
 // Endfile
