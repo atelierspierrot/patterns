351c34638ec96292d8ef49f29746441532bd7e43
Piero Wbmstr <me@e-piwi.fr>
2013-05-30 17:30:07 +0200

    New statis creator feature

---
+++ src/Patterns/Interfaces/StaticCreatorInterface.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PHP patterns
+ * Copyleft (c) 2013 Pierre Cassat and contributors
+ * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
+ * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * Sources <https://github.com/atelierspierrot/patterns>
+ */
+
+namespace Patterns\Interfaces;
+
+/**
+ * This interface allows to create objects that can be statically created "on-the-fly" to allow
+ * method chaining constructing them like:
+ *
+ *     $obj = ObjectStaticCreator::create( arguments )
+ *         ->method();
+ *
+ * @author      Piero Wbmstr <piero.wbmstr@gmail.com>
+ */
+interface StaticCreatorInterface
+{
+
+    /**
+     * @return self Must return a new instance of the object created with function arguments
+     */
+    public static function create();
+    
+}
+
+// Endfile
