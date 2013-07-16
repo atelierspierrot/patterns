a36b69ffcd3020a1ba8fdc4b9064ba50e73e1175
Piero Wbmstr <me@e-piwi.fr>
2013-07-16 17:05:07 +0200

    Updating class documentation and rearranging methods

--- src/Patterns/Commons/ConfigurationRegistry.php
+++ src/Patterns/Commons/ConfigurationRegistry.php
@@ -1,20 +1,38 @@
 <?php
 /**
- * PHP/Apache/Markdown DocBook
- * @package 	DocBook
- * @license   	GPL-v3
- * @link      	https://github.com/atelierspierrot/docbook
+ * PHP patterns
+ * Copyleft (c) 2013 Pierre Cassat and contributors
+ * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
+ * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * Sources <https://github.com/atelierspierrot/patterns>
  */
 
 namespace Patterns\Commons;
 
 /**
+ * A manager for a configuration registry as an array with a depth of 2
+ * 
+ * This class handles a simple and classic registry of values (stored in an array
+ * as `key=>value` pairs) except that it accepts and manages the first depth keys
+ * of the array like a `scope` and permits to access values within scopes directly.
+ * 
+ * The rule here is that when you used to call `$obj->get(property)` you can now call
+ * `$obj->get(scope:property)` ; this is very useful for configuration entries, that
+ * are often stored in complex arrays identified by a simple key.
+ * 
+ * @author     	Piero Wbmstr <piero.wbmstr@gmail.com>
  */
 class ConfigurationRegistry
 {
 
+	/**
+	 * @var array
+	 */
     protected $registry;
 
+	/**
+	 * Construction : initialization of the registry on an empty array
+	 */
     public function __construct()
     {
         $this->registry = array();
@@ -23,9 +41,10 @@ class ConfigurationRegistry
     /**
      * Set the value of a specific option with depth
      *
-     * @param string $name The index of the configuration value to get, with a depth using notation `index:name`
+     * @param string $name The index of the configuration value to get, with a scope using notation `index:name`
      * @param misc $value The value to set for $name
-     * @param string $scope The scope to use in the configuration registry
+     * @param string $scope The scope to use in the configuration registry if it is not defined in the `$name` parameter
+     * 
      * @return self Returns `$this` for method chaining
      */
     public function set($name, $value, $scope = null)
@@ -43,9 +62,10 @@ class ConfigurationRegistry
     /**
      * Get the value of a specific option with depth
      *
-     * @param string $name The index of the configuration value to get, with a depth using notation `index:name`
-     * @param misc $default The default value to return if so
-     * @param string $scope The scope to use in the configuration registry
+     * @param string $name The index of the configuration value to get, with a scope using notation `index:name`
+     * @param misc $default The default value to return if so (`null` by default)
+     * @param string $scope The scope to use in the configuration registry if it is not defined in the `$name` parameter
+     * 
      * @return misc The value retrieved in the registry or the default value otherwise
      */
     public function get($name, $default = null, $scope = null)
@@ -62,8 +82,9 @@ class ConfigurationRegistry
     /**
      * Set an array of options
      *
-     * @param array $options The array of values to set for the configuration
-     * @param string $scope The scope to use in the configuration registry
+     * @param array $options The array of values to set for the configuration entry
+     * @param string $scope The scope to use in the configuration registry (optional)
+     * 
      * @return self Returns `$this` for method chaining
      */
     public function setConfigs(array $options, $scope = null)
@@ -78,11 +99,12 @@ class ConfigurationRegistry
     }
 
     /**
-     * Set the value of a specific option
+     * Set the value of a specific option (no scope notation allowed here)
      *
      * @param string $name The index of the configuration value
      * @param misc $value The value to set for the configuration entry
-     * @param string $scope The scope to use in the configuration registry
+     * @param string $scope The scope to use in the configuration registry (optional)
+     * 
      * @return self Returns `$this` for method chaining
      */
     public function setConfig($name, $value, $scope = null)
@@ -110,7 +132,12 @@ class ConfigurationRegistry
     }
 
     /**
-     * Get the array of options
+     * Get the array of options (from a specific scope if so)
+     * 
+     * @param misc $default The default value to return if so (`null` by default)
+     * @param string $scope The scope to use in the configuration registry (optional)
+     * 
+     * @return array|misc|null
      */
     public function getConfigs($default = null, $scope = null)
     {
@@ -121,14 +148,19 @@ class ConfigurationRegistry
     }
 
     /**
-     * Get the value of a specific option
+     * Get the value of a specific option (no scope notation allowed here)
+     *
+     * @param string $name The index of the configuration value to get
+     * @param misc $default The default value to return if so (`null` by default)
+     * @param string $scope The scope to use in the configuration registry (optional)
+     * 
+     * @return misc
      */
     public function getConfig($name, $default = null, $scope = null)
     {
         if (!is_null($scope)) {
             return isset($this->registry[$scope][$name]) ? $this->registry[$scope][$name] : $default;
         }
-
         return isset($this->registry[$name]) ? $this->registry[$name] : $default;
     }
 
