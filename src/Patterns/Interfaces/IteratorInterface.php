8cf92e1c0190bd61f780010b7d232c399e880697
Piero Wbmstr <me@e-piwi.fr>
2014-06-14 09:59:47 +0200

    new piwi github username

--- src/Patterns/Interfaces/IteratorInterface.php
+++ src/Patterns/Interfaces/IteratorInterface.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * PHP patterns
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
- * Sources <https://github.com/atelierspierrot/patterns>
+ * Sources <http://github.com/atelierspierrot/patterns>
  */
 
 namespace Patterns\Interfaces;
@@ -16,7 +16,7 @@ use \SeekableIterator;
  *
  * This interface is just set for homogeneity in iterations and array walking.
  *
- * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @author 		Piero Wbmstr <me@e-piwi.fr>
  */
 interface IteratorInterface
     extends SeekableIterator
