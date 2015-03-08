c0230a4b57e49a0ff5dbbfa1f94a45a59981fd39
piwi <me@e-piwi.fr>
2015-03-08 11:59:14 +0100

    re-introduce the WebPageInterface (deleted by merge)

---
+++ src/Patterns/Interfaces/WebPageInterface.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file is part of the Patterns package.
+ *
+ * Copyright (c) 2013-2015 Pierre Cassat <me@e-piwi.fr> and contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The source code of this package is available online at 
+ * <http://github.com/atelierspierrot/patterns>.
+ */
+
+namespace Patterns\Interfaces;
+
+/**
+ * This work is BETA (not fully tested)
+ *
+ * @author  Piero Wbmstr <me@e-piwi.fr>
+ */
+interface WebPageInterface
+{
+
+    /**
+     * Build the page menu
+     */
+    public function buildMenu();
+
+    /**
+     * Get the page menu
+     */
+    public function getMenu();
+
+    /**
+     * Build the page title
+     */
+    public function buildTitle();
+
+    /**
+     * Get the page title
+     */
+    public function getTitle();
+
+    /**
+     * Build the page metas
+     */
+    public function buildMetas();
+
+    /**
+     * Get the page metas
+     */
+    public function getMetas();
+
+    /**
+     * Get one page meta
+     */
+    public function getMeta($name);
+
+}
+
+// Endfile
